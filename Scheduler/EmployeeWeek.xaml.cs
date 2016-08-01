d1cd9280e257c8a0b73bce5e33fd6517b019bb95
Kant0sh <freddyface@freenet.de>
2016-08-01 17:52:43 +0200

    Add project files.

---
+++ Scheduler/EmployeeWeek.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scheduler
+{
+    /// <summary>
+    /// Interaction logic for EmployeeWeek.xaml
+    /// </summary>
+    public partial class EmployeeWeek : UserControl
+    {
+
+        private EmployeeDay[] days;
+
+        private int dayCount;
+        private int startDay;
+
+        public EmployeeWeek(int dayCount, int startDay)
+        {
+
+            this.dayCount = dayCount;
+            this.startDay = startDay;
+
+            days = new EmployeeDay[dayCount];
+
+            for(int i = 0; i < days.Length; i++)
+            {
+                days[i] = new EmployeeDay();
+            }
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ Scheduler/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scheduler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            slider.ValueChanged += Slider_ValueChanged;
+            this.SizeChanged += MainWindow_SizeChanged;
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(ColumnDefinition col in grid.ColumnDefinitions)
+            {
+                col.MinWidth = this.ActualWidth / grid.ColumnDefinitions.Count;
+            }
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            label.Content = "" + slider.Value * this.ActualWidth / 10;
+            foreach (ColumnDefinition col in grid.ColumnDefinitions)
+            {
+                col.Width = new GridLength(slider.Value * this.ActualWidth / 10);
+                col.MinWidth = this.ActualWidth / grid.ColumnDefinitions.Count;
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            label.Content = "" + slider.Value * this.ActualWidth/10;
+            foreach (ColumnDefinition col in grid.ColumnDefinitions)
+            {
+                col.Width = new GridLength(slider.Value*this.ActualWidth/10);
+            }
+        }
+    }
+}
